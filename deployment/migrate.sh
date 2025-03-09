c815b1a4fc3fabe4eab26c46bf287eaaa94a86bd
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2025-03-09 13:03:14 +0900

    Update migrate.sh

--- deployment/migrate.sh
+++ deployment/migrate.sh
@@ -2,13 +2,13 @@
 
 echo "Please enter your domain name: "
 read domainName
-cd jackett
+cd jackett_new
 sudo rm -rf data
 sudo docker compose down
-cd ../addon
+cd ../addon_new
 sudo rm -rf data
 sudo docker compose down
-cd ../traefik
+cd ../traefik_new
 sudo docker compose down
 cd ..
 
@@ -18,20 +18,20 @@ sudo iptables -A INPUT -p tcp --dport 9217 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8291 -j ACCEPT
 
 sudo netfilter-persistent save
-cd traefik
+cd traefik_new
 sudo docker compose up -d
-cd ../jackett
+cd ../jackett_new
 sudo mkdir data blackhole
 sudo curl -fsSL https://raw.githubusercontent.com/aymene69/stremio-jackett/main/deployment/jackett/docker-compose.yml -o ./docker-compose.yml
 sudo sed -i "s/YOURADDON.COM/$domainName/g" ./docker-compose.yml
 sudo docker compose up -d
 
-cd ../addon
+cd ../addon_new
 sudo curl -fsSL https://raw.githubusercontent.com/aymene69/stremio-jackett/main/deployment/docker-compose-traefik.yml -o ./docker-compose.yml
 sudo sed -i "s/YOURADDON.COM/$domainName/g" ./docker-compose.yml
 sudo docker compose pull
 sudo docker compose up -d
-cd ../traefik
+cd ../traefik_new
 sudo docker compose up -d
 clear
 IP=$(curl -4 -s ifconfig.me)
